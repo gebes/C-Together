63a8312a35013d924773c957cb580f70b67344cb
Christoph Krassnigg <christoph_krassnigg@gmx.net>
2020-04-19 12:36:22 +0200

    Added Münchhausen-Zahl

---
+++ 001 Q3-203 Münchhausen-Zahl/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int isMuenchhausenNumber(int number);
+
+int main() {
+
+    int input;
+    printf("Bitte eine Zahl eingeben: ");
+    scanf("%d", &input);
+
+    int result = isMuenchhausenNumber(input);
+    printf("Die Zahl %d ist %seine Muenchhausen-Zahl", input, (result ? "" : "k"));
+
+    return 0;
+}
+
+int isMuenchhausenNumber(int numberToTest) {
+    unsigned int sum = 0;
+
+    int digits = numberToTest;
+
+    while (digits) {
+        int currentDigit = digits % 10;
+        sum += pow(currentDigit, currentDigit);
+        digits /= 10;
+    }
+
+    return sum == numberToTest;
+}
+
