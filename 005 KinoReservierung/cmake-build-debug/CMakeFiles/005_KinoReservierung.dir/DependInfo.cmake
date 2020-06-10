28364762ecfccf1961de5c1f044aa4b6e0d7a569
Christoph Krassnigg <35232234+Gebes@users.noreply.github.com>
2020-06-10 10:07:13 +0200

    Created KinoReservierung

---
+++ 005 KinoReservierung/cmake-build-debug/CMakeFiles/005_KinoReservierung.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/christoph/Library/Mobile Documents/com~apple~CloudDocs/Schule/POS1/C-Together/005 KinoReservierung/main.cpp" "/Users/christoph/Library/Mobile Documents/com~apple~CloudDocs/Schule/POS1/C-Together/005 KinoReservierung/cmake-build-debug/CMakeFiles/005_KinoReservierung.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 005 KinoReservierung/cmake-build-debug/CMakeFiles/005_KinoReservierung.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "005_KinoReservierung"
+  "005_KinoReservierung.pdb"
+  "CMakeFiles/005_KinoReservierung.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/005_KinoReservierung.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
